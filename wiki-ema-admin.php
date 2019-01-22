759483ed789ca7fa056ed34e6709df6cb7d9599b
hgodinho <hnrq.godinho@gmail.com>
2019-01-22 18:52:32 -0200

    testes

--- wiki-ema-admin.php
+++ wiki-ema-admin.php
@@ -2,4 +2,18 @@
 
 echo '<h1>Wiki-Ema</h1>';
 
+$args = array(
+    'post_type' => 'obras'
+);
+
+$obras_sync = new WP_Query( $args );
+
+//print_r( $obras_sync );
+
+foreach( $obras_sync as $obra_sync ){
+    $autor = get_post_meta(get_the_id());
+    print_r( $autor );
+}
+
+
 ?>
